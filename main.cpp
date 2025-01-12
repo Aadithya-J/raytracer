b148402ea52b16a68f8c8f9f52626f28379c729b
Aadithya-J <jlaadithya@gmail.com>
2025-01-12 23:26:39 +0530

    formatting code and removing main executable

--- main.cpp
+++ main.cpp
@@ -5,18 +5,22 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
 
     int image_width = 256;
     int image_height = 256;
 
-    cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    cout << "P3\n"
+         << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = 0; j < image_height; j++) {
+    for (int j = 0; j < image_height; j++)
+    {
         clog << "\rScanlines remaining: " << (image_height - j) << ' ' << flush;
-        for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
+        for (int i = 0; i < image_width; i++)
+        {
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
             auto b = 0.0;
             auto pixel_color = color(r, g, b);
             write_color(cout, pixel_color);
