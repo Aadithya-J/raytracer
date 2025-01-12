43f61390e7678a6cd60a31f08bd428bd1333d499
Aadithya-J <jlaadithya@gmail.com>
2025-01-12 23:43:17 +0530

    use ray class

--- main.cpp
+++ main.cpp
@@ -1,15 +1,36 @@
 #include "color.h"
 #include "vec3.h"
+#include "ray.h"
 
 #include <iostream>
 
 using namespace std;
 
+color ray_color(const ray& r) {
+    return color(100,100,100);
+}
+
 int main()
 {
+    double aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
 
-    int image_width = 256;
-    int image_height = 256;
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    auto focal_length = 1.0;
+    double viewport_height = 2.0;
+    double viewport_width = viewport_height * (double(image_width)/image_height);
+    auto camera_center = point3(0, 0, 0);
+
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     cout << "P3\n"
          << image_width << ' ' << image_height << "\n255\n";
@@ -19,10 +40,10 @@ int main()
         clog << "\rScanlines remaining: " << (image_height - j) << ' ' << flush;
         for (int i = 0; i < image_width; i++)
         {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.0;
-            auto pixel_color = color(r, g, b);
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+            color pixel_color = ray_color(r);
             write_color(cout, pixel_color);
         }
     }
